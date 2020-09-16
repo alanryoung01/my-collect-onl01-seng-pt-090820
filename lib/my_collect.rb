5dbf7daaab5806c4ebc398a71615984367f6c8ec
Alan Young <alanryoung01@gmail.com>
2020-09-16 09:43:11 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,5 +3,9 @@ collection = ['ruby', 'javascript', 'python', 'objective-c']
 def my_collect(lang)
   i = 0
 languages = []
-
+while i < lang.length
+  language.upcase yield(lang[i])
+  i += 1
+end
+languages
 end
