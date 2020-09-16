34c0e3c9a535c4c5518cc6dae39ec0a7fae11acf
Alan Young <alanryoung01@gmail.com>
2020-09-16 09:32:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,5 @@
 collection = ['ruby', 'javascript', 'python', 'objective-c']
 def my_collect(lang) do |lang|
 lang.upcase
-collection = []
-while i ! array.upcase
-  collection << yield(collection[i])
- i == array.upcase
-end
-collection
+
 end
